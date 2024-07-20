e4f9784d38555e05d09191dfa7e362d21c326abe
deyvisonalcantara <deyvisonalcantara@gmail.com>
2024-07-19 21:51:28 -0300

    Lista de tarefas

---
+++ COLLECTIONS-JAVA-API-2024/src/OperacoesBasicas/List/ListaTarefa.java
@@ -0,0 +1,54 @@
+package OperacoesBasicas.List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    //atributo
+    private List<Tarefa> tarefalist;
+
+    public ListaTarefa() {
+       this.tarefalist = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao) {
+        tarefalist.add(new Tarefa(descricao));
+    }
+    
+    public void removerTarefa(String descricao) {
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for(Tarefa t : tarefalist) {
+            if (t.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefasParaRemover.add(t); 
+            }
+        }
+        tarefalist.removeAll(tarefasParaRemover);
+    } 
+
+    public int obterNumeroTotalTarefas() {
+        return tarefalist.size();
+    }
+
+    public void obterDescricoesTarefas() {
+        System.out.println(tarefalist);
+    }
+
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
+
+        System.out.println( "O numero total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.adicionarTarefa( "Limpar casa");
+        listaTarefa.adicionarTarefa( "varrer");
+        listaTarefa.adicionarTarefa( "lavar banheiro");
+        System.out.println( "O numero total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.removerTarefa("varrer");
+        System.out.println( "O numero total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.obterDescricoesTarefas();
+
+    }
+
+}
+ 
